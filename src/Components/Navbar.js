d081f95ed45d24ee17d059de1b98f18ed3d9a8dc
princesharma-coder <princewarrior683@gmail.com>
2024-10-12 01:26:21 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Navbar({ mode, toggleMode}) {
+export default function Navbar({ mode, toggleMode }) {
   return (
-    <nav className={`navbar d-flex justify-content-center navbar-expand-lg navbar-${mode} bg-${mode}`}>
-      <div className="container-fluid d-flex justify-content-md-between">
-        <a className="navbar-brand position-relative start-30 mx-3" href="/">TextUtills</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-          <div className={`form-check form-switch text-${mode === 'light' ? 'dark' : 'light'} ms-auto`}>
-            <input
-              className="form-check-input position-relative end-100"
-              type="checkbox"
-              role="switch"
-              id="flexSwitchCheckDefault"
-              onChange={toggleMode}
-              checked={mode === 'dark'}
-            />
-            <label className="form-check-label  position-relative end-100" htmlFor="flexSwitchCheckDefault">
-              {mode === 'light' ? 'Enable Dark Mode' : 'Enable Light Mode'}
-            </label>
+    <nav className={`navbar navbar-expand-lg d-flex justify-content-between navbar-${mode} bg-${mode}`}>
+      <div className="container-fluid">
+        <a className="navbar-brand" href="/">TextUtills</a>
+
+        {/* Dark/Light mode toggle switch */}
+        <div className={`form-check form-switch text-${mode === 'light' ? 'dark' : 'light'}`}>
+          <input
+            className="form-check-input"
+            type="checkbox"
+            role="switch"
+            id="flexSwitchCheckDefault"
+            onChange={toggleMode}
+            checked={mode === 'dark'}
+          />
+          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+            {mode === 'light' ? 'Enable Dark Mode' : 'Enable Light Mode'}
+          </label>
         </div>
+      </div>
+    </nav>
+  );
+}
+
+// PropTypes for type checking
+Navbar.propTypes = {
+  mode: PropTypes.string.isRequired,
+  toggleMode: PropTypes.func.isRequired,
+};
+
       </div>
     </nav>
   )
